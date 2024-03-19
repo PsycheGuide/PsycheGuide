ecf928e7489786fb46b756beb08ca155943e9574
Akiroze <daryl.mike.d@gmail.com>
2024-03-19 23:14:06 +0800

    Nadagdagan ng comments hahahaha

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PsycheGuide
+
 A project requirement for Pangasinan State University Urdaneta Campus' computer engineering AY 2023-2024, class 2C's Software subject
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+""""""
+
+@app.route("/home")
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/forms")
+def forms():
+    return render_template("Forms.html")
+
+
+
+""""""
+if __name__=="__main__":
+    app.run(debug=True)
